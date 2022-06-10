39c1316e31dfc54f41df9fc406ad787b30e3c4ba
= <>
2022-06-10 16:59:14 -0500

    Hice responsive el catalogo v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from './Components/Admin/productos/productos.component';
+import { RegistroComponent } from './Components/Client/registro/registro.component';
 import { CatalogoComponent } from './Components/Index/catalogo/catalogo.component';
 import { IniciarSesionComponent } from './Components/Index/iniciar-sesion/iniciar-sesion.component';
 import { AuthGuard } from './Guards/auth.guard';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'productos', component: ProductosComponent, canActivate: [AuthGuard] },
   { path: 'iniciarSesion', component: IniciarSesionComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: '**', pathMatch: 'full', redirectTo: '/catalogo' }
 ];
 
