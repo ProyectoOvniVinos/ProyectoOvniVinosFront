28657b25390dc10d94a2578229851c0d6fa62cb9
ivan <ivanhshs1@gmail.com>
2022-06-17 16:02:23 -0500

    agregando carrito

--- src/app/Components/Client/carrito/carrito.component.ts
+++ src/app/Components/Client/carrito/carrito.component.ts
@@ -1,9 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-carrito',
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
+  
 })
 export class CarritoComponent implements OnInit {
 
@@ -11,5 +41,12 @@ export class CarritoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  isOpen = true;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+  
 
 }
