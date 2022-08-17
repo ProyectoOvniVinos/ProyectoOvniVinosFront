d55bcb2842ca2093b347ac5f5913cecae7a39d16
Brayni Campo <braynivalentina@gmail.com>
2022-08-17 00:29:09 -0500

    Falta organizar la tabla

--- src/app/Components/Modal/modal-inventario-g/modal-inventario-g.component.ts
+++ src/app/Components/Modal/modal-inventario-g/modal-inventario-g.component.ts
@@ -3,6 +3,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ProductoModel } from 'src/app/Models/Producto.model';
 import { Inventario_detallesModel } from 'src/app/Models/Inventario_detalles.model';
 import { DialogData } from '../DialogData';
+import { DatePipe, DATE_PIPE_DEFAULT_TIMEZONE } from '@angular/common';
 
 @Component({
   selector: 'app-modal-inventario-g',
@@ -11,7 +12,27 @@ import { DialogData } from '../DialogData';
 })
 export class ModalInventarioGComponent implements OnInit {
 
+  datosRegistro: Inventario_detallesModel;
+  datos: Inventario_detallesModel[] = [
+    {
+      idDetalles: 3,
+      cantidadProducto: 4,
+      fechaUltimoIngreso: new Date(),
+    },
 
+    {
+      idDetalles: 6,
+      cantidadProducto: 12,
+      fechaUltimoIngreso: new Date ("12-08-2022"),
+    },
+
+    {
+      idDetalles: 8,
+      cantidadProducto: 3,
+      fechaUltimoIngreso: new Date ("12-08-2022"),
+    },
+    
+  ];
   
   constructor(    
     public dialogRef: MatDialogRef<ModalInventarioGComponent>,
