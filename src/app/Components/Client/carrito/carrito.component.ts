7993b20f6a854d1ac80d05edc01d7dd9c2c5b57c
= <>
2022-08-22 16:31:39 -0500

    Validacion cantidad carrito

--- src/app/Components/Client/carrito/carrito.component.ts
+++ src/app/Components/Client/carrito/carrito.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, Input, OnChanges, EventEmitter , Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { CarritoClienteModel } from 'src/app/Models/CarritoCliente.model';
 import { ClienteModel } from 'src/app/Models/Cliente.model';
+import { Inventario_generalModel } from 'src/app/Models/Inventario_general.model';
 import { ItemCarritoModel } from 'src/app/Models/itemCarrito.model';
 import { ProductoModel } from 'src/app/Models/Producto.model';
 import { CarritoService } from 'src/app/Services/carrito.service';
 import { ClienteService } from 'src/app/Services/cliente.service';
+import { InventarioGService } from 'src/app/Services/inventario-g.service';
+import { ModalInteraccionComponent } from '../../Modal/modal-interaccion/modal-interaccion.component';
 
 @Component({
   selector: 'app-carrito',
@@ -15,7 +19,9 @@ import { ClienteService } from 'src/app/Services/cliente.service';
 export class CarritoComponent implements OnInit, OnChanges {
 
   carrito:CarritoClienteModel;
-  variable:boolean = true
+  variable:boolean = true;
+  advertirCantidad = false;
+  cantidadP:number = 0;
 
   @Input() modal:boolean = false;
 
@@ -24,7 +30,8 @@ export class CarritoComponent implements OnInit, OnChanges {
   @Output()
   devolver = new EventEmitter<any>();
 
-  constructor(private clienteService:ClienteService, private carritoService:CarritoService) {
+ 
+  constructor(private clienteService:ClienteService, private carritoService:CarritoService, private inventarioService:InventarioGService,public dialog: MatDialog) {
 
   }
   ngOnChanges(): void {
@@ -68,21 +75,59 @@ export class CarritoComponent implements OnInit, OnChanges {
     this.devolver.emit(list);
   }
   aumentarCantidad(item:ItemCarritoModel){
-    
-    item.cantidadProducto += 1
-    
-    this.carritoService.actualizarCarrito(this.carrito).subscribe(resp=>{
-      console.log(resp);
+    this.inventarioService.getInventarioGeneralByProducto(item.codigoProducto.codigoProducto).subscribe((resp:Inventario_generalModel)=>{
+      this.cantidadP = resp.cantidadProducto
       
-    });
-  }
-  disminuirCantidad(item:ItemCarritoModel){
-    item.cantidadProducto-=1;
-    this.carritoService.actualizarCarrito(this.carrito).subscribe(resp=>{
-      console.log(resp);
-      
-    });
+      if(this.cantidadP>item.cantidadProducto){
+        item.cantidadProducto += 1
+        this.advertirCantidad = false;
+        console.log(this.cantidadP + "disponible");
+        console.log(item.cantidadProducto + "solicitada");
+        
+        this.carritoService.actualizarCarrito(this.carrito).subscribe(resp=>{
+          console.log(resp);
+          
+        });
+      }else{
+        console.log(this.cantidadP + "disponible");
+        console.log(item.cantidadProducto + "solicitada");
+        console.log("BBBBBBBBBBBBBBBBBB");
+        this.advertirCantidad = true;
+
+      }
+    })
+
   }
   
+  disminuirCantidad(item:ItemCarritoModel){
+    if(item.cantidadProducto==1){
+      this.openDialogInteraction("Advertencia",`Eliminara el producto de su carrito. ¿Desea eliminarlo?`, item);
+    }else{
+      this.advertirCantidad = false;
+      item.cantidadProducto-=1;
+      
+      this.carritoService.actualizarCarrito(this.carrito).subscribe(resp=>{
+        console.log(resp);
+        
+      });
+    }
+    
+    
+  }
+  openDialogInteraction(titleNew: string, mensajeNew: string, item:ItemCarritoModel):void{
+    const dialogRef = this.dialog.open(ModalInteraccionComponent, {
+      width: '300px',
+      data: {title: titleNew, mensaje: mensajeNew},
+    });
+    dialogRef.afterClosed().subscribe( (result:boolean) => {
+      console.log(`Dialog result: ${result}`); // Pizza!
+      if(result==true){
+        this.eliminarItem(item);
+      }else{
+        console.log("en else");
+        
+      }
+    });
+  }
 
 }
