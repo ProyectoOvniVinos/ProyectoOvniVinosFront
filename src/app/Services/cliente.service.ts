a1ece3b0258f8bd774db9b61baccb7b9444fea9c
Cristian Amador <crissis2004@gmail.com>
2022-08-27 23:21:33 -0500

    Pull

--- src/app/Services/cliente.service.ts
+++ src/app/Services/cliente.service.ts
@@ -8,45 +8,45 @@ import { ClienteModel } from '../Models/Cliente.model';
 })
 export class ClienteService {
 
-  url:string="http://localhost:8080/apiCliente/";
+  url: string = "http://localhost:8080/apiCliente/";
 
-  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
   constructor(private http: HttpClient) { }
-  registro(cliente:ClienteModel): Observable<any>{
+  registro(cliente: ClienteModel): Observable<any> {
     console.log(cliente);
-    
-    return this.http.post<any>(`${this.url}registro`, cliente, {headers: this.httpHeaders})
-    .pipe(
-      catchError(e => {
 
-        if(e.status==500){
+    return this.http.post<any>(`${this.url}registro`, cliente, { headers: this.httpHeaders })
+      .pipe(
+        catchError(e => {
+
+          if (e.status == 500) {
+            return throwError(e);
+          }
+
           return throwError(e);
-        }
-
-        return throwError(e);
-      })
-    );
+        })
+      );
   }
-  getAll(){
+  getAll() {
     return this.http.get<any>(`${this.url}clientes`);
   }
-  getByEmail(email:String){
+  getByEmail(email: String) {
     return this.http.get<any>(`${this.url}cliente/${email}`);
   }
 
-  recuperarPassword(url:String,correoEncrypt:string){
-    let data:any={
-      url:url,
-      correoEncrypt:correoEncrypt,
-      correo:this.desencriptar(correoEncrypt)
+  recuperarPassword(url: String, correoEncrypt: string) {
+    let data: any = {
+      url: url,
+      correoEncrypt: correoEncrypt,
+      correo: this.desencriptar(correoEncrypt)
     }
-    return this.http.post<any>(`${this.url}recuperar`, data, {headers: this.httpHeaders})
+    return this.http.post<any>(`${this.url}recuperar`, data, { headers: this.httpHeaders })
   }
-  desencriptar(correo:string){
+  desencriptar(correo: string) {
     let desencriptado = atob(correo)
     return desencriptado;
   }
-  actualizar(cliente:ClienteModel){
-    return this.http.put<any>(`${this.url}cliente/${cliente.correoCliente}`,cliente,{headers: this.httpHeaders})
+  actualizar(cliente: ClienteModel) {
+    return this.http.put<any>(`${this.url}cliente/${cliente.correoCliente}`, cliente, { headers: this.httpHeaders })
   }
 }
