b40060b6d9cba900ac2fb169106e637d9a3401c7
ivan <ivanhshs1@gmail.com>
2022-08-29 15:07:49 -0500

    validators en cambio de contraseña

--- src/app/Components/Client/cambio-password-c/cambio-password-c.component.ts
+++ src/app/Components/Client/cambio-password-c/cambio-password-c.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cambio-password-c',
@@ -6,10 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cambio-password-c.component.css']
 })
 export class CambioPasswordCComponent implements OnInit {
+  cambioForm: FormGroup;
+  banderaPasswordTwo: boolean ;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.crearFormulario();
   }
 
+  get passwordOneControl(): FormControl{
+    return this.cambioForm.get('passwordOne') as FormControl
+  }
+
+  get passwordTwoControl(): FormControl{
+    return this.cambioForm.get('passwordTwo') as FormControl
+  }
+
+
+  get passwordOneNoValido() {
+    if(this.cambioForm.get('passwordOne')?.touched){
+      if(this.cambioForm.get('passwordOne')?.invalid == false){
+        return false;
+      }else{
+        return true;
+      }
+    }else{
+      return null;
+    }
+
+  }
+
+  get passwordTwoNoValido() {
+    const contrasena1 = this.cambioForm.get('passwordOne')?.value;
+    const contrasena2 = this.cambioForm.get('passwordTwo')?.value;
+    if(this.cambioForm.get('passwordTwo')?.touched){
+      if(this.cambioForm.get('passwordTwo')?.invalid == false){
+        if(contrasena1 === contrasena2){
+          this.banderaPasswordTwo=true;
+          return false;
+
+        }else{
+          this.banderaPasswordTwo=false;
+          return true;
+        }
+      }else{
+        return true;
+      }
+    }else{
+      return null;
+    }
+
+  }
+
+  crearFormulario() {
+    this.cambioForm = this.fb.group({
+      passwordOne: ['', [Validators.required, Validators.minLength(8)]],
+      passwordTwo: ['', Validators.required]
+    })
+  }
 }
